0d8a9652c6500c7b4118741e846a603967be3ecf
AbouCloud <Abouzari6839@gmail.com>
2025-08-05 00:35:25 +0000

    project updated

--- src/Project.jsx
+++ src/Project.jsx
@@ -3,62 +3,87 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 import Pimg1 from "../src/assets/image/Gestion.jpg";
+import { FaEye } from "react-icons/fa";
+
 
 function Project() {
     useEffect(() => {
-                AOS.init({
-                  duration: 1500, // animation duration in ms
-                  once: true      // animation only happens once on scroll
-                });
-              }, []);
+        AOS.init({
+            duration: 1500, // animation duration in ms
+            once: true      // animation only happens once on scroll
+        });
+    }, []);
     const project = [
         {
             id: 1,
-            Name: "Gestion Des Etudiants",
             Link: "https://abouzari.lovestoblog.com/",
             image: Pimg1,
-            h1: "ENGLISH LANGUAGE LABORATORY",
-            p: "A school management web app for the English Language Laboratory, built with HTML, CSS, and JavaScript. It includes a dashboard for teachers and admins to manage students and track their performance."
+            h1: "Complete School Management Portal with Admin Dashboard",
         },
         {
             id: 2,
-            Name: "Gestion Des Etudiants",
-            Link: "https://abouzari.lovestoblog.com/",
-            image: Pimg1,
-            h1: "ENGLISH LANGUAGE LABORATORY",
-            p: "A school management web app for the English Language Laboratory, built with HTML, CSS, and JavaScript. It includes a dashboard for teachers and admins to manage students and track their performance."
+            Link: "https://as-code.wuaze.com/search&filter/",
+            image: "filter.png",
+            h1: "RESEACH ITEM OR FILTER",
         },
         {
             id: 3,
-            Name: "Gestion Des Etudiants",
-            Link: "https://abouzari.lovestoblog.com/",
-            image: Pimg1,
-            h1: "ENGLISH LANGUAGE LABORATORY",
-            p: "A school management web app for the English Language Laboratory, built with HTML, CSS, and JavaScript. It includes a dashboard for teachers and admins to manage students and track their performance."
+            Link: "https://as-code.wuaze.com/To-do-list/To-do_list.html",
+            image: "to-do.png",
+            h1: "Vanilla JS To-Do App with Local Storage",
+        },
+        {
+            id: 4,
+            Link: "https://abzar12.github.io/abouzari-sweat-food/",
+            image: "restaurant.png",
+            h1: "Restaurant SWEAT-MEAL",
+        },
+        {
+            id: 5,
+            Link: "https://as-code.wuaze.com/index.html",
+            image: "formcontact.png",
+            h1: "Let’s talk about your website — contact us here",
+        },
+        {
+            id: 6,
+            Link: "https://abzar12.github.io/End-of-day-Challenges/Drum%20kit/drum.html",
+            image: "drum.png",
+            h1: "KeyBoardBeats",
+        },
+        {
+            id: 7,
+            Link: "https://as-code.wuaze.com/image-slide/image-slide.html",
+            image: "imageSlide.png",
+            h1: "Image Slide Example",
+        },
+        {
+            id: 8,
+            Link: "https://abzar12.github.io/End-of-day-Challenges/playing-css&js/var.html",
+            image: "var.png",
+            h1: "Customizing Styles with JavaScript and CSS Root Variables",
         }
     ]
     return (
         <>
-            <div className="conatainer m-5">
-                <div className="ac_row w-full  text-white p-4 rounded min-h-[300px]">
+            <div className="conatainer">
+                <div className="">
                     <h1 className="text-3xl font-[Lora] text-blue-600 text-center">PROJETS</h1>
-                    <div className=" w-full  p-5 mx-auto" >
+                    <div className="ac-items" >
                         {
                             project.map((ele) => (
-                                <div className="mx-auto mb-20 bg-[#001227] shadow-[0_0_30px_rgba(255,255,255,0.2)] max-w-[900px]" >
-                                    <ul className="">
-                                        <li key={ele.id} className="">
-                                            <a href={ele.Link} className="mx-auto sm:flex" target="_blank" rel="noopener noreferrer" data-aos="fade-right"><img className="sm:max-w-[350px] " src={ele.image} key={ele.id} alt="Gestion_Etudiant" />
-                                                <div className=" mx-5 sm:mt-5">
-                                                    <h1 className="font-[Lora] underline font-bold text-1xl md:text-2xl ">{ele.h1}</h1>
-                                                    <p className=" font-[Roboto] text-gray-400 ">{ele.p}</p>
-                                                </div>
-                                            </a>
-
-                                        </li>
-                                    </ul>
-                                </div>
-
+                                    <div className="item" >
+                                        <ul className="">
+                                            <a href={ele.Link} className="z-50" target="_blank" rel="noopener noreferrer" data-aos="fade-right">
+                                            <li key={ele.id} className="">
+                                                <img className="" src={ele.image} key={ele.id} alt="Gestion_Etudiant" />
+                                                    <div className="">
+                                                        <h1 className="font-[Lora] text-center">{ele.h1}</h1>
+                                                    </div>
+                                                     < FaEye className="my-eye" />
+                                            </li>
+                                                </a>
+                                        </ul>
+                                    </div>
                             ))
                         }
                     </div>
