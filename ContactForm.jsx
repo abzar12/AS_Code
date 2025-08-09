c9a3aa993bf8b6dce9ae0032dd52a3c0f49aadd1
AbouCloud <Abouzari6839@gmail.com>
2025-08-09 19:27:35 +0000

    contact form to manage and see Customers

---
+++ ContactForm.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+function GetData() {
+    const [data, SetData] = useState([]);
+
+    useEffect(() => {
+            fetch("https://as-code.wuaze.com/GetDataContact.php")
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Getting Data failed");
+                    }
+                    return response.json();
+                })
+                .then((data) => SetData(data))
+        .catch((error) => {
+            console.error(error.message)
+        });
+    }, [])
+    console.log(data);
+    return (
+        <>
+
+        </>
+    )
+}
+export default GetData;
